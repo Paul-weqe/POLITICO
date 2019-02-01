0d03d9452639ebfebaa4ff7d147d09a33995556c
Paul-weqe <paul1tw1@gmail.com>
2019-02-01 14:29:28 +0300

    editing navbar links to various pages

---
+++ js/styles.js
@@ -0,0 +1,13 @@
+var login = document.getElementById("submit-login");
+
+login.addEventListener("click", function(){
+    // alert("Cool");
+    window.open("view-profile.html");
+});
+
+
+var register = document.getElementById("submit-register");
+
+register.addEventListener("click", function(){
+    window.open("user-login.html");
+});
